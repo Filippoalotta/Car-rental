ba7c0a22b5cc582d504d639d9c109891143a779d
Filippoalotta <alottafilippo@gmail.com>
2024-04-23 21:34:56 +0200

    finita pagina about

--- src/assets/components/WhyChooseUs.jsx
+++ src/assets/components/WhyChooseUs.jsx
@@ -10,7 +10,6 @@ function WhyChooseUs() {
                 <h4>Why Choose Us</h4>
                 <h2>Choose, Save, Drive</h2>
                 <p>With a wide selection of high-quality vehicles, competitive rates, and impeccable customer service, we are committed to ensuring that every journey is a success. Our dedication to convenience and customer satisfaction sets us apart from the competition.</p>
-                <button className='button2'>Learn More</button>
             </div>
             <img src={whyChooseUs} alt="Why choose us image" />
         </section>
--- src/assets/pages/AboutPage.jsx
+++ src/assets/pages/AboutPage.jsx
@@ -1,5 +1,8 @@
 import '../scss/AboutPage.scss'
 import Header from '../components/Header';
+import OurStory from '../images/our-story.jpg'
+import EasyFastService from '../components/EasyFastServices';
+import WhyChooseUs from '../components/WhyChooseUs';
 import Footer from '../components/Footer';
 
 function AboutPage(){
@@ -8,7 +11,19 @@ function AboutPage(){
         <>
             <Header></Header>
             <div className='about-page'>
-                <h1>About Page</h1>
+                <div className="page-bg">
+                    <h2>Our Team</h2>
+                </div>
+                <div className="our-story">
+                    <img src={OurStory} alt="Why choose us image" />
+                    <div className="our-story-content">
+                        <h4>Our Story</h4>
+                        <h2>Between Past and Future</h2>
+                        <p>Through challenges and triumphs, our car rental story has unfolded. With passion and dedication, we&apos;ve transformed travelers&apos; needs into unique experiences. Looking ahead, we&apos;ll continue to drive towards new horizons, carrying our commitment to excellence with us.</p>
+                    </div>
+                </div>
+                <EasyFastService></EasyFastService>
+                <WhyChooseUs></WhyChooseUs>
             </div>
             <Footer></Footer>
         </>
